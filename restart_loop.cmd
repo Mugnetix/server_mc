8c2e00b17aaf6762d6004dd5e338fddb1856c071
Kirill Koshelev <kk17012002@gmail.com>
2025-03-16 22:44:32 +0100

    World update

--- restart_loop.cmd
+++ restart_loop.cmd
@@ -1,6 +1,6 @@
 @echo OFF
 :start
-    "C:\Java\adoptiumjdk-21.0.5+11-jre\bin\java.exe" -Xms8G -Xmx8G -XX:+UnlockExperimentalVMOptions -XX:+UseZGC -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:+PerfDisableSharedMem -XX:-ZUncommit -XX:+ParallelRefProcEnabled -Xlog:async @libraries/net/minecraftforge/forge/1.19.2-43.4.0/win_args.txt nogui
+    "C:\Java\adoptiumjdk-21.0.5+11-jre\bin\java.exe" -Xms10G -Xmx10G -XX:+UnlockExperimentalVMOptions -XX:+UseZGC -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:+PerfDisableSharedMem -XX:-ZUncommit -XX:+ParallelRefProcEnabled -Xlog:async @libraries/net/minecraftforge/forge/1.19.2-43.4.0/win_args.txt nogui
     SET /p should_restart=<./auto_restart/restart
     if %should_restart% EQU 0 goto stop
     echo If you want to stop the server completely, press Ctrl-C, \before the countdown is at 1!
