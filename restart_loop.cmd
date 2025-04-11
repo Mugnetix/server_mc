99aaa6116e92373ec0641eee0fbcd115dab1a67d
Kirill Koshelev <kk17012002@gmail.com>
2025-04-11 15:39:01 +0200

    World update

--- restart_loop.cmd
+++ restart_loop.cmd
@@ -1,6 +1,6 @@
 @echo OFF
 :start
-    "C:\Java\adoptiumjdk-21.0.5+11-jre\bin\java.exe" -Xms8G -Xmx8G -XX:+UnlockExperimentalVMOptions -XX:+UseZGC -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:+PerfDisableSharedMem -XX:-ZUncommit -XX:+ParallelRefProcEnabled -Xlog:async @libraries/net/minecraftforge/forge/1.19.2-43.4.0/win_args.txt nogui
+    "C:\Java\adoptiumjdk-21.0.5+11-jre\bin\java.exe" -Xms6G -Xmx6G -XX:+UnlockExperimentalVMOptions -XX:+UseZGC -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:+PerfDisableSharedMem -XX:-ZUncommit -XX:+ParallelRefProcEnabled -Xlog:async @libraries/net/minecraftforge/forge/1.19.2-43.4.0/win_args.txt nogui
     rem SET /p should_restart=<./auto_restart/restart
 	SET should_restart=0
     if %should_restart% EQU 0 goto stop
